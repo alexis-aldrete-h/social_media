f8c8289e9321f2e6c04b97df01c84a456b7ef8a5
Alexis Aldrete <aaldrete@insulet.com>
2024-05-02 23:41:13 -0700

    TASK 2: Adding Home Page and Navbar

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+        <nav className="navbar">
+            <div className="navbar-logo">
+                <h1>MyBlog</h1>
+            </div>
+            <div className="navbar-links">
+                <a href="#">Home</a>
+                <a href="#">Blog</a>
+                <a href="#">About</a>
+                <a href="#">Contact</a>
+            </div>
+            <div className="navbar-buttons">
+                <button className="btn-signup">Sign Up</button>
+                <button className="btn-login">Login</button>
+            </div>
+        </nav>
+    </header>
+  );
+};
+
+export default Header;
